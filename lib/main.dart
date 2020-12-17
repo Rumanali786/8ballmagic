fee9eee29c5179e8b59065d537a9028ba0c2e1f1
DESKTOP-7IHTN2H\ruman <jutts1055@gmail.com>
2020-12-17 19:44:28 +0500

    8 ballpool magic app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,48 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Ask Me Anything"),
+            backgroundColor: Colors.indigo,
+          ),
+          body: ballPool(),
+          backgroundColor: Colors.blue,
+        ),
       ),
     );
+// ignore: camel_case_types
+class ballPool extends StatefulWidget {
+  @override
+  _ballPoolState createState() => _ballPoolState();
+}
+
+// ignore: camel_case_types
+class _ballPoolState extends State<ballPool> {
+  int number = 1;
+  void incrementOperator(){
+      setState(() {
+        number++;
+        number=number%5+1;
+      }
+      );
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Expanded(child: FlatButton(
+        onPressed: () {
+
+          incrementOperator();
+      },
+        child: Image(image: AssetImage("images/ball$number.png"),
+        ),
+      ),
+      ),
+      );
+
+  }
+}
+
+
